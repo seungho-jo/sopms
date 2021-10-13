24c8ac03ced4bb5262a51893a5d915b27cfebb8f
naSeyoung <sktpdud97@naver.com>
2021-10-13 23:53:21 +0900

    회원등록추가,페이징기능추가

--- src/main/java/sopms/User/controller/userController.java
+++ src/main/java/sopms/User/controller/userController.java
@@ -20,18 +20,39 @@ public class userController {
 		d.addAttribute("list",service.getUserList(name));
 		return "WEB-INF\\view\\userList.jsp";
 	}
-		/*		@RequestMapping("userListAjax.do") 
-	public String userList2(Model d,String name) { //직원리스트
+		
+	/*	@RequestMapping("userListAjax.do") 
+		public String userList2(Model d,String name) { //직원리스트
 		d.addAttribute("list",service.getUserList(name));
 		return "WEB-INF\\view\\userList.jsp"; 
 	}
 	*/
+	
 	//http://localhost:7080/sopms/pageList.do
 	@RequestMapping("pageList.do") //페이징 처리
 	public String userList2(userSch sch,Model d) { 
 		d.addAttribute("list",service.getUserListPaging(sch));
 		return "WEB-INF\\view\\userList.jsp";
 	}  
+	     
+	@RequestMapping("Insertpage.do") //사용자등록버튼 누르면 페이지이동
+	public String Insertpage() {
+		return "WEB-INF\\view\\userRegister.jsp";
+	}
+	
+	
+	//http://localhost:7080/sopms/memberInsert.do
+	@RequestMapping("memberInsert.do") //사용자등록버튼 눌렀을때 db처리 
+	public String regMemberForm(User user, Model d) {
+		System.out.println(user.getId()+":,"+user.getPass()+","+user.getName()+","+user.getDept());
+		service.insertUser(user); //성공했을때
+			
+		return "redirect:/pageList.do";
+	}
+
+		
+	
+	
 	
 	
 	
--- src/main/java/sopms/User/dao/userDao.java
+++ src/main/java/sopms/User/dao/userDao.java
@@ -9,6 +9,6 @@ public interface userDao {
 	public ArrayList<User> userList(String name); //String name
 	public int totCnt(userSch sch);
 	public ArrayList<User> userListPaging(userSch sch);
-	
+	public void insertUser(User ins); //사용자초기등록
 	
 }
