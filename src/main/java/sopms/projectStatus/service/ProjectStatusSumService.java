d75416764a49fc593bc011347121057e113db6c6
GarlicButterShrimp <dukang2012@gmail.com>
2021-10-14 22:47:54 +0900

    pj status fix

--- src/main/java/sopms/projectStatus/service/ProjectStatusSumService.java
+++ src/main/java/sopms/projectStatus/service/ProjectStatusSumService.java
@@ -11,16 +11,26 @@ import sopms.projectStatus.dao.ProjectStatusSumDao;
 public class ProjectStatusSumService { 
 	@Autowired
 	private ProjectStatusSumDao sumDao;
-	private Gson gson;
 	
 	public String projectInfoJson(int pcode) {
-		gson = new Gson();
-		String infoJson = gson.toJson(sumDao.projectInfo(pcode));
-		String deptJson = gson.toJson(sumDao.projectDeptList(pcode));
-		String mergedJson = "{\"info\":"+infoJson+",\"dept\":"+deptJson+"}";
-		System.out.println("infoJson: "+infoJson);
-		System.out.println("deptJson: "+deptJson);
-		System.out.println("merge: "+mergedJson);
-		return mergedJson;
+		Gson gson = new Gson();
+		StringBuilder sb = new StringBuilder();
+		// project table 데이터를 json 문자열로 변환
+		sb.append(
+				gson.toJson(sumDao.projectInfo(pcode))
+		);
+		// project_dept table 속성을 json 문자열에 추가
+		sb.insert(
+				sb.length()-1,
+				",\"dept\":"
+		);
+		// project_dept table 속성값(배열)을 json 문자열에 추가
+		sb.insert(
+				sb.length()-1,
+				gson.toJson(sumDao.projectDeptList(pcode))
+		);
+		//개행문자 변환
+		String projectInfoJson = new String(sb);
+		return projectInfoJson.replace("\\r\\n", "<br>");
 	}
 }
