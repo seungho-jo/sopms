35f58c8d567f8ead797a66921d7cc1b9eeaf95b2
gwanhun <leegawnhun@gmail.com>
2021-10-14 22:22:21 +0900

    gantt chart 수정 프로젝트리스트 수정

--- src/main/java/sopms/Management/controller/managementController.java
+++ src/main/java/sopms/Management/controller/managementController.java
@@ -22,14 +22,33 @@ public class managementController {
 						Model d, Project_List_paging project_Lp) {
 		HttpSession session = request.getSession();
 		User user = (User)session.getAttribute("user");
-		project_Lp.setPm(user.getId());
-		d.addAttribute("list",service.getProjectList(project_Lp));
-		return "WEB-INF\\view\\project_list.jsp";
+		
+		if (!user.getRank().equals("부장")) {
+			d.addAttribute("msg", "접근권한이 없습니다.");
+			return "WEB-INF\\view\\main.jsp";
+		} else {
+			project_Lp.setPm(user.getId());
+			d.addAttribute("list",service.getProjectList(project_Lp));
+			return "WEB-INF\\view\\project_list.jsp";
+		}
+		
 	}
 	
 	// http://localhost:7080/sopms/status.do
-	@RequestMapping("status.do")
-	public String projectStatus() {
-		return "WEB-INF\\view\\project_status.jsp";
-	}
+		@RequestMapping("status.do")
+		public String projectStatus() {
+			return "WEB-INF\\view\\project_status.jsp";
+		}
+		
+		// http://localhost:7080/sopms/status01.do
+		@RequestMapping("status01.do")
+		public String projectStatus01() {
+			return "WEB-INF\\view\\ganttchart.jsp";
+		}
+		// http://localhost:7080/sopms/wbslist.do
+		@RequestMapping("wbslist.do")
+		public String wbslist(Model d) {
+			d.addAttribute("list",service.wbslist());
+			return "pageJsonReport";
+		}
 }
--- src/main/java/sopms/Management/dao/managementDao.java
+++ src/main/java/sopms/Management/dao/managementDao.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 import sopms.vo.Project_List;
 import sopms.vo.Project_List_paging;
+import sopms.vo.Work;
 
 
 
 public interface managementDao {
 	public int projectCount(String pm);
 	public ArrayList<Project_List> projectList(Project_List_paging project_Lp);
+	public ArrayList<Work> wbslist();
 }
 
